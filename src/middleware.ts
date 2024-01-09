61e84ad29ec2ae86717adafbf368138f635102b9
Yousef <yahmed1996a@gmail.com>
2024-01-09 14:00:54 +0000

    Wrap app in auth provider and add middleware so every route (bar two) are protected

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: ["/", "/contact"],
+});
+ 
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
