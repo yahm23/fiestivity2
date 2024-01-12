25c51e9ec5cb2090a01fb91a85de5863325a8e04
Yousef <yahmed1996a@gmail.com>
2024-01-12 13:43:22 +0000

    Create rough template for event page

---
+++ src/pages/event/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import LayoutGeneral from '~/components/ui/layout-general';
+
+
+export default function Event () {
+  return (
+    <LayoutGeneral>
+        Event!
+    </LayoutGeneral>
+  );
+}
