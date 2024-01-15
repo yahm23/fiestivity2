fd187a17a6bfc94bf82f3e3b535e84aa0b4c21ca
Yousef <yahmed1996a@gmail.com>
2024-01-15 15:24:49 +0000

    Create event page, listing user events

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,11 +1,30 @@
+import { useUser } from '@clerk/nextjs';
 import * as React from 'react';
 import LayoutGeneral from '~/components/ui/layout-general';
-
+import { api } from "~/utils/api";
 
 export default function Event () {
+  const {user, isLoaded} = useUser();
+  if (!isLoaded) return (<div>Loading...</div>)
+  if (!user) return (<div>Something went wrong!</div>)
+
+  const {data, isLoading} = api.event.getUserEventsFromUserId.useQuery({userId: user.id});
+
+  if (isLoading) return (<div>Loading....</div>)
+  if (!data) return (<div>Something went wrong!</div>)
   return (
     <LayoutGeneral>
-        Event!
+        <div>
+          Your events:
+          <div>
+            {data.events.map((event)=>(
+              <div key={event.id}>
+                {event.name}
+                {event.content}
+              </div>
+            ))}
+          </div>
+        </div>
     </LayoutGeneral>
   );
 }
